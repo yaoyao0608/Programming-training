f64123c0c320deb3abaef3b7e484adb1352364d1
yaoyao0608 <nicole_dlut@163.com>
2017-03-29 22:29:08 +0800

    add

--- learn-python-the-hard-way/ex26.py
+++ learn-python-the-hard-way/ex26.py
@@ -1,4 +1,4 @@
-#import  ex25 
+
 def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
@@ -8,15 +8,15 @@ def sort_words(words):
     """Sorts the words."""
     return sorted(words)
 
-def print_first_word(words)   # 加上":"
+def print_first_word(words):   
     """Prints the first word after popping it off."""
-    word = words.poop(0)  #pop(0)
-    print word
+    word = words.pop(0)  
+    print (word)
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
-    word = words.pop(-1   #加上“）”
-    print word
+    word = words.pop(-1)   
+    print (word)
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
@@ -35,8 +35,8 @@ def print_first_and_last_sorted(sentence):
     print_first_word(words)
     print_last_word(words)
 
-print "Let's practice everything."
-print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+print ("Let's practice everything.")
+print ('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
 
 poem = """
 \tThe lovely world
@@ -47,42 +47,42 @@ and requires an explantion
 \n\t\twhere there is none.
 """
 
-print "--------------"
-print poem
-print "--------------"
+print ("--------------")
+print (poem)
+print ("--------------")
 
-five = 10 - 2 + 3 - 5
-print "This should be five: %s" % five
+five = 10 - 2 + 3 - 6
+print ("This should be five: %s" % five)
 
 def secret_formula(started):
     jelly_beans = started * 500
-    jars = jelly_beans \ 1000  #除法是“/”
+    jars = jelly_beans / 1000  
     crates = jars / 100
     return jelly_beans, jars, crates
 
 start_point = 10000
-beans, jars, crates == secret_formula(start-point)  # jelly_beans =  start_point 
+beans, jars, crates = secret_formula(start_point)
 
-print "With a starting point of: %d" % start_point
-print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
+print ("With a starting point of: %d" % start_point)
+print ("We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates))
 
 start_point = start_point / 10
 
-print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont   #  start_point)
+print ("We can also do that this way:")
+print ("We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point))
 
-sentence = "All god\tthings come to those who weight."  #good things
+sentence = "All good things come to those who weight."  
 
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
+words = break_words(sentence)
+sorted_words = sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
-.print_first_word(sorted_words)   # 去掉print前面的 .
-print_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
-prin sorted_words   # print
+print_first_word(sorted_words)
+print_last_word(sorted_words)   
+sorted_words = sort_sentence(sentence)
+print (sorted_words)
 
-print_irst_and_last(sentence)  # print print_first_and_last
+print_first_and_last(sentence)  
 
-   print_first_a_last_sorted(senence)   #indent print first_a_last_sorted(sentence)
+print_first_and_last_sorted(sentence) 
